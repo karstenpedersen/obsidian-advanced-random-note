fa3cf637896e643c676b1b8812443e72c674aa8d
Mateusz Jabłoński <mateusz.piotr.jablonski@gmail.com>
2024-02-14 20:51:50 +0100

    Add ribbon action setting

--- src/settings.ts
+++ src/settings.ts
@@ -1,14 +1,15 @@
 import { App, PluginSettingTab, Setting } from "obsidian";
 import QueryView from "src/gui/queryItem/QueryView.svelte";
 import AdvancedRandomNote from "./main";
-import { type OpenType, type Query } from "./types";
-import { getOpenTypeLabels, toRecord } from "./utilities";
+import { RibbonActionType, type OpenType, type Query } from "./types";
+import { getOpenTypeLabels, getRibbonActionTypeLabels, toRecord } from "./utilities";
 
 export interface Settings {
 	queries: Array<Query>;
 	disabledFolders: string;
 	debug: boolean;
 	openType: OpenType;
+	ribbonActionType: RibbonActionType;
 	setActive: boolean;
 }
 
@@ -17,6 +18,7 @@ export const DEFAULT_SETTINGS: Settings = {
 	disabledFolders: "",
 	debug: false,
 	openType: "Active Leaf",
+	ribbonActionType: "Open random note",
 	setActive: true,
 };
 
@@ -74,6 +76,20 @@ export class SettingTab extends PluginSettingTab {
 						await this.plugin.saveSettings();
 					});
 			});
+
+		// Ribbon action type
+		new Setting(this.containerEl)
+		.setName("Ribbon action type")
+		.setDesc("Which action to perform after tapping ribbon button.")
+		.addDropdown((dropdown) =>
+			dropdown
+				.addOptions(toRecord(getRibbonActionTypeLabels()))
+				.setValue(this.plugin.settings.ribbonActionType)
+				.onChange(async (value) => {
+					this.plugin.settings.ribbonActionType = value as RibbonActionType;
+					await this.plugin.saveSettings();
+				})
+		);
 	}
 
 	addQueriesSetting() {
